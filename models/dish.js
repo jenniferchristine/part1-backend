914be751e1f7812572676b5111e342a4e0f9d205
jenniferchristine <jeja2306@student.miun.se>
2024-08-10 22:32:42 +0200

    added messages for dish mongoose validations

--- models/dish.js
+++ models/dish.js
@@ -2,13 +2,45 @@ const mongoose = require("mongoose");
 
 // egenskaper för alla maträtter
 const dishSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }, // maträttens namn
-    description: { type: String, required: true }, // beskrivning av maträtt
-    price: { type: Number, required: true }, // pris för rätt
-    category: { type: String, required: true },  // kategori (ska den va med?)
-    ingredients: [{ type: String }], // ingredienser förhållande till ingrediensschemat
-    created: { type: Date, default: Date.now }
+    name: { 
+        type: String, 
+        required: [true, "Maträtten måste ha ett namn"], 
+        trim: true,
+        minlength: [3, "Namnet måste vara minst 3 tecken långt"],
+        maxlength: [25, "Namnet får inte vara längre än 25 tecken långt"],
+        unique: true 
+    }, 
+    description: { 
+        type: String, 
+        required: [true, "Ange en beskrivning av maträtten"], 
+        minlength: [10, "Beskrivning måste vara minst 10 tecken lång"],
+        maxlength: [60, "Beskrivningen får inte vara längre än 60 tecken"]
+    }, 
+    price: { 
+        type: Number, 
+        required: [true, "Du måste ange ett pris för maträtten"], 
+    }, 
+    category: { 
+        type: String, 
+        required: [true, "Maträtten måste kategoriseras"] 
+    },  
+    ingredients: [{ // fixa modell till detta för antal och mängd? 
+        type: String, 
+        required: [true, "Du måste ange maträttens ingredienser"]
+    }], 
+    created: { 
+        type: Date, default: Date.now 
+    }
 });
+
+// virtuell egenskap som visar kr efter pris
+dishSchema.virtual("currency").get(function() {
+    return this.price + "kr";
+});
+
+// inkluderar virtuella fält vid konvertering till json
+dishSchema.set("toJSON", { virtuals: true });
+dishSchema.set("toOBJECT", { virtuals: true });
 
 const Dish = mongoose.model("Dish", dishSchema); // inkludera till databas
 module.exports = Dish; // exporterar modellen så den kan användas i andra filer
