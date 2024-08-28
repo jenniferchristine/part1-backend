3b1951c010a3a15f50b8876b304b7c95a7686fe4
jenniferchristine <jeja2306@student.miun.se>
2024-08-28 09:52:32 +0200

    improved middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (!token) return res.status(401).json({ message: "Token missing - Not authorized" });
+    const authHeader = req.headers['authorization']; // hämta header från request
+    const token = authHeader && authHeader.split(' ')[1]; // extrahera den
 
+    if (!token) {
+        return res.status(401).json({ message: "Token is missing: Access denied" });
+    }
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-        if (err) return res.status(403).json({ message: "Not correct token" });
+
+        if (err) {
+            return res.status(403).json({ message: "Invalid token - Access denied" }); // kod för åtkomst förbjuden
+        }
         req.user = user;
         next();
     });
